532471db4fa5a4845a442e9583f2f95bc89e8e40
Jean de Colombel <decolombeljean@gmail.com>
2016-08-22 19:39:54 +0200

    I did some corrections on the fields in models

--- db/migrate/20160822173346_create_job_applications.rb
+++ db/migrate/20160822173346_create_job_applications.rb
@@ -1,8 +1,8 @@
 class CreateJobApplications < ActiveRecord::Migration[5.0]
   def change
     create_table :job_applications do |t|
-      t.reference :candidate
-      t.reference :job_offer
+      t.references :candidate
+      t.references :job_offer
       t.text :cover_letter
 
       t.timestamps
