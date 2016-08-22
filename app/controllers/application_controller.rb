65672a3835332457c56b032c49df51b2ef13ee74
Guillaume Corlay <guillaume.corlay-van-der-schaal@hec.edu>
2016-08-22 17:22:42 +0200

    added candidate & recruiter models with their signins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_candidate!
+  # before_action :authenticate_candidate!, except: {controllers: [devise]}
+
+  before_action :authenticate_candidate!, unless: :devise_controller?
+  before_action :authenticate_recruiter!, unless: :devise_controller?
 end
+
+# Solution 1
+#   before_action :authenticate_candidate!, unless: :devise_controller?
+#   before_action :authenticate_recruiter!, unless: :devise_controller?
+# Dans les controller skip_before_action :authenticate_recruiter (quand on est sur une action candidat)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :recruiters
   devise_for :candidates
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
