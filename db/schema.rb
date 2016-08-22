d34b76a99b7824a3d2cd1ad872a117f82cb67f3f
Vincent Bres <bres.vincent@gmail.com>
2016-08-22 19:52:52 +0200

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822173346) do
+ActiveRecord::Schema.define(version: 20160822174951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 20160822173346) do
     t.index ["recruiter_id"], name: "index_job_offers_on_recruiter_id", using: :btree
   end
 
+  create_table "languages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "proficiency"
+    t.integer  "job_application_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["job_application_id"], name: "index_languages_on_job_application_id", using: :btree
+  end
+
   create_table "recruiters", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -67,4 +76,5 @@ ActiveRecord::Schema.define(version: 20160822173346) do
     t.index ["reset_password_token"], name: "index_recruiters_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "languages", "job_applications"
 end
