74c701dc849f852f138a5c25aca752e252e6d99b
Brice Tessier Huort <tessierhuort@gmail.com>
2016-08-23 15:10:11 +0200

    manage navbar and actions job-offers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_candidate!, only: [ :home ]
+  skip_before_action :authenticate_recruiter!, only: [ :home ]
 
   def home
   end
