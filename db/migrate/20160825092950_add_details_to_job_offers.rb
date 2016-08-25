61022c5a9a2bb2c7166c4cdc55d45f7fa9660e8c
Brice Tessier Huort <tessierhuort@gmail.com>
2016-08-25 11:40:25 +0200

    add details to job offer

---
+++ db/migrate/20160825092950_add_details_to_job_offers.rb
@@ -0,0 +1,7 @@
+class AddDetailsToJobOffers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :job_offers, :title, :string
+    add_column :job_offers, :contract, :string
+    add_column :job_offers, :location, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822191340) do
+ActiveRecord::Schema.define(version: 20160825092950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,17 +43,18 @@ ActiveRecord::Schema.define(version: 20160822191340) do
     t.boolean  "present"
     t.string   "degree_type"
     t.string   "degree_field"
-    t.string   "organization"
+    t.integer  "organization_id"
     t.integer  "job_application_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.index ["job_application_id"], name: "index_educations_on_job_application_id", using: :btree
+    t.index ["organization_id"], name: "index_educations_on_organization_id", using: :btree
   end
 
   create_table "experiences", force: :cascade do |t|
     t.integer  "company_size"
     t.string   "industry"
-    t.string   "organization"
+    t.integer  "organization_id"
     t.integer  "job_application_id"
     t.string   "title"
     t.date     "start_date"
@@ -63,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160822191340) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.index ["job_application_id"], name: "index_experiences_on_job_application_id", using: :btree
+    t.index ["organization_id"], name: "index_experiences_on_organization_id", using: :btree
   end
 
   create_table "job_applications", force: :cascade do |t|
@@ -80,6 +82,9 @@ ActiveRecord::Schema.define(version: 20160822191340) do
     t.text     "description"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "title"
+    t.string   "contract"
+    t.string   "location"
     t.index ["recruiter_id"], name: "index_job_offers_on_recruiter_id", using: :btree
   end
 
@@ -121,7 +126,9 @@ ActiveRecord::Schema.define(version: 20160822191340) do
   end
 
   add_foreign_key "educations", "job_applications"
+  add_foreign_key "educations", "organizations"
   add_foreign_key "experiences", "job_applications"
+  add_foreign_key "experiences", "organizations"
   add_foreign_key "languages", "job_applications"
   add_foreign_key "recruiters", "organizations"
 end
