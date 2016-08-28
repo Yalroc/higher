bca0ed3f5d687c62601607b1d1bfbd2a501d0fa3
Vincent Bres <bres.vincent@gmail.com>
2016-08-28 13:09:48 +0200

    migration done

--- db/migrate/20160828093849_add_omniauth_to_candidates.rb
+++ db/migrate/20160828093849_add_omniauth_to_candidates.rb
@@ -2,8 +2,6 @@ class AddOmniauthToCandidates < ActiveRecord::Migration[5.0]
   def change
     add_column :candidates, :provider, :string
     add_column :candidates, :uid, :string
-    add_column :candidates, :first_name, :string
-    add_column :candidates, :last_name, :string
     add_column :candidates, :token, :string
     add_column :candidates, :token_expiry, :datetime
   end
