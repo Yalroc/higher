f10a121f91c649fd6d8a049c02f22b85a82e6ddc
Vincent Bres <bres.vincent@gmail.com>
2016-08-28 17:01:22 +0200

    I don't know where invalid credentials comes from

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -8,11 +8,11 @@ class Candidate < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:linkedin]
 
  def self.find_for_linkedin_oauth(auth)
-  user_params = auth.to_h.slice(:provider, :uid)
-  user_params.merge! auth.info.slice(:email, :first_name, :last_name)
-  # user_params[:facebook_picture_url] = auth.info.image
-  user_params[:token] = auth.credentials.token
-  user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
+  current_candidate_params = auth.to_h.slice(:provider, :uid)
+  current_candidate_params.merge! auth.info.slice(:email, :first_name, :last_name)
+  current_candidate_params[:facebook_picture_url] = auth.info.image
+  current_candidate_params[:token] = auth.credentials.token
+  current_candidate_params[:token_expiry] = Time.at(auth.credentials.expires_at)
 
   user = Candidate.where(provider: auth.provider, uid: auth.uid).first
   user ||= Candidate.where(email: auth.info.email).first # User did a regular sign up in the past.
